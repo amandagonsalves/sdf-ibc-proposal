89c5cf17ee6812fa65f3c51935325c2dbb42d32d
Amanda Gonsalves <64379712+amandagonsalves@users.noreply.github.com>
2026-05-16 23:15:14 -0300

    feat(gateway): add proto definitions and tracing
    * add initial protobuf message definitions for gateway operations
    
    * add tracing subscriber setup to gateway main function
    
    * delete automatic staging of rust files from pre-commit hook

--- crates/gateway/src/main.rs
+++ crates/gateway/src/main.rs
@@ -1,1 +1,10 @@
-fn main() {}
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
+
+pub mod proto;
+
+fn main() {
+    tracing_subscriber::registry()
+        .with(EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info")))
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+}
